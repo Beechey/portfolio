40394ad169e64211548b8e873870833d54c84a2a
Matt Beechey <m.beechey@lboro.ac.uk>
2024-11-08 19:55:31 +0000

    Add section dividers

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 import { links } from "@/lib/data";
 import Link from "next/link";
 
-export default function Header() {
+export default function Nav() {
   return (
     <header className="z-[999] relative">
       <motion.div
