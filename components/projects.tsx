2d5cb23739606b4bda48572bbf28baa3d0e238d1
Matt Beechey <m.beechey@lboro.ac.uk>
2024-11-09 12:58:46 +0000

    Change publications to projects

---
+++ components/projects.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SectionHeading from "./section_heading";
+
+export default function Projects() {
+  return (
+    <section
+      id="projects"
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-32">
+      <SectionHeading>Projects and publications</SectionHeading>
+
+      <div></div>
+    </section>
+  );
+}
