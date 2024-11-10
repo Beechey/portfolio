7772ffdd1cdff58eb6f987d01d1549cd9575fbb0
Matt Beechey <m.beechey@lboro.ac.uk>
2024-11-10 16:53:47 +0000

    Begin Experience section

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import SectionHeading from "./section_heading";
 
 export default function Experience() {
-  return <div>Experience</div>;
+  return (
+    <section
+      id="experience"
+      className="mb-28 max-w-[50rem] sm:mb-0 scroll-mt-32">
+      <SectionHeading>My Experience</SectionHeading>
+    </section>
+  );
 }
