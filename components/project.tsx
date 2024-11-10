d9d6d8864a3972b9bd9b5167149c037a6fa9b104
Matt Beechey <m.beechey@lboro.ac.uk>
2024-11-10 16:05:27 +0000

    Fixed projects issue where on small screens the image overlapped

--- components/project.tsx
+++ components/project.tsx
@@ -29,37 +29,35 @@ export default function Project({
         scale: scaleProgress,
         opacity: opacityProgress,
       }}
-      className="group sm:mb-8 last:mb-0">
-      <section
-        id="projects"
-        className="relative bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 sm:h-[20rem] group-even:pl-8 hover:bg-gray-50 transition rounded-lg">
-        <div className="flex flex-col pt-4 pb-8 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] h-full group-even:ml-[18rem]">
-          <h3 className="text-2xl font-semibold">{title}</h3>
-          <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
-          <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
-            {tags.map((tag, index) => (
-              <li
-                className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
-                key={index}>
-                {tag}
-              </li>
-            ))}
-          </ul>
-        </div>
+      className="group mb-8 last:mb-0">
+      <a
+        href={link}
+        target="_blank">
+        <section
+          id="projects"
+          className="relative bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 sm:h-[20rem] sm:group-even:pl-8 hover:bg-gray-50 transition rounded-lg">
+          <div className="flex flex-col pt-4 pb-8 pl-10 pr-2 sm:pt-10 sm:max-w-[50%] h-full sm:group-even:ml-[18rem]">
+            <h3 className="text-2xl font-semibold">{title}</h3>
+            <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
+            <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+              {tags.map((tag, index) => (
+                <li
+                  className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
+                  key={index}>
+                  {tag}
+                </li>
+              ))}
+            </ul>
+          </div>
 
-        <a href={link}>
           <Image
-            className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:-right-[initial] group-even:-left-40 group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-hover:scale-105 transition
-        
-        group-even:group-hover:translate-x-3 
-        group-even:group-hover:translate-y-3 
-        group-even:group-hover:rotate-2"
+            className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-hover:scale-105 transition group-even:group-hover:translate-x-3 group-even:-right-[initial] group-even:-left-40 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2"
             src={image}
             alt={description}
             quality={95}
           />
-        </a>
-      </section>
+        </section>
+      </a>
     </motion.div>
   );
 }
--- components/skills.tsx
+++ components/skills.tsx
@@ -1,40 +1,26 @@
+"use client";
+
 import React from "react";
 import SectionHeading from "./section_heading";
+import { skillsData } from "@/lib/data";
 
 export default function Skills() {
+  // const ref = useSectionInView("Skills");
+
   return (
     <section
       id="skills"
-      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-32">
-      <SectionHeading>Skills</SectionHeading>
-      <p className="">
-        I am a <span className="font-bold">collaborative</span> and driven
-        machine learning engineer with a strong foundation in technical skills,
-        as well as research and product management. After earning my PhD from{" "}
-        <span className="font-bold">Loughborough University</span>, where my
-        research on evidential classification in network security led to
-        <span className="font-bold"> high-impact publications</span>, I gained
-        experience in machine learning areas such as neural networks and
-        time-series forecasting. My recent role as a Technical Product Owner for
-        an IoT project allowed me to{" "}
-        <span className="font-bold">lead embedded engineering innovations</span>{" "}
-        for public transport, utilising my skills in hardware design, machine
-        learning, and stakeholder engagement to improve passenger experience.
-        <br />
-        <br />
-        In addition to technical expertise, I have completed{" "}
-        <span className="underline">advanced training</span> in{" "}
-        <span className="font-bold">leadership</span>,{" "}
-        <span className="font-bold">strategic innovation</span>, and{" "}
-        <span className="font-bold">project management</span>, which complement
-        my proficiency in cloud computing, embedded systems, and programming
-        languages like Python and C. I am known for a{" "}
-        <span className="underline">hands-on approach</span> to problem-solving
-        and continue to contribute to{" "}
-        <span className="font-bold">cutting-edge</span> research in Beyond 5G
-        networks, presenting my work at industry conferences and expanding my
-        impact across both academic and commercial domains.
-      </p>
+      className="max-w-[53rem] scroll-mt-28 text-center">
+      <SectionHeading>My skills</SectionHeading>
+      <ul className="flex flex-wrap justify-center gap-2 text-md text-gray-800">
+        {skillsData.map((skill, index) => (
+          <li
+            className="bg-white border botder-black/[0.1] rounded-xl px-5 py-3"
+            key={index}>
+            {skill}
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
