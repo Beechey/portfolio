ea054bd2d4872fec0f6da77ab3f405507974b884
Matt Beechey <m.beechey@lboro.ac.uk>
2024-11-17 16:16:02 +0000

    Remove old imports and functions from utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,3 @@
-import { ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
 export const validateString = (value: unknown) => {
   if (!value || typeof value !== "string") {
     return false;
