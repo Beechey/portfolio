416f8182e01feb5260f4c9d12687b8793273aeb2
Matt Beechey <m.beechey@lboro.ac.uk>
2024-11-17 15:40:17 +0000

    function to validate strings

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const  validateString = (value: unknown) => {
+  if (!value || typeof value !== "string") {
+    return false;
+  }
+  
+  return true;
+}
