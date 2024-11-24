8e9febfa32c2fedf79ca2168f7cb55acf8d8c431
Matt Beechey <m.beechey@lboro.ac.uk>
2024-11-24 17:31:21 +0000

    Email styling

--- email/contact-form-email.tsx
+++ email/contact-form-email.tsx
@@ -24,12 +24,12 @@ export default function ContactFormEmail({
   return (
     <Html>
       <Head />
-      <Preview>New message from Portfiolio</Preview>
+      <Preview>New message from Portfolio ({email})</Preview>
       <Tailwind>
-        <Body>
+        <Body className="bg-gray-100">
           <Container>
-            <Section>
-              <Heading>
+            <Section className="bg-white border border-black my-10 px-10 py-4 rounded-md">
+              <Heading className="leading-tight">
                 You received the following message from your contact form
               </Heading>
               <Text>{message}</Text>
